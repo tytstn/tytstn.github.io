6caae26a8735eeb36c89d0f77c44cb9f8be15e89
tytstn <41092094+tytstn@users.noreply.github.com>
2019-02-27 23:34:44 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Chart from './my-chart'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Chart></Chart>
+      </div>
+    );
+  }
+}
+
+export default App;
